7f7b94f8ff5226985360b1996c362f8910e09ec6
Seunghyun Kim <sevencuter@gmail.com>
2024-12-17 14:27:57 +0900

    My First Kaggle Study commit

---
+++ Intro-to-programming/Question4.py
@@ -0,0 +1,13 @@
+#Question 4
+#In the tutorial, you defined several variables to calculate the total number of seconds in a year. Run the next code cell to do the calculation here.
+
+# Create variables
+num_years = 4
+days_per_year = 365 
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+
+# Calculate number of seconds in four years
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
