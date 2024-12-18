07076c122a7d24958c91a3006ee948695d28b68b
Seunghyun Kim <seunghyunkim@Seunghyunui-MacBookPro.local>
2024-12-18 22:25:20 +0900

    Kaggle Data_Types

--- Intro-to-programming/Data_Types.py
+++ Intro-to-programming/Data_Types.py
@@ -56,3 +56,15 @@ def get_expected_cost(beds:int, baths:int, has_basement:bool):
     basement_cost = 40000 * int(has_basement)
     value = base_cost  + beds_cost + baths_cose + basement_cost
     return value
+
+
+# 🌶️ Question 5
+# You own an online shop where you sell rings with custom engravings. You offer both gold plated and solid gold rings.
+
+# Gold plated rings have a base cost of $50, and you charge $7 per engraved unit.
+# Solid gold rings have a base cost of $100, and you charge $10 per engraved unit.
+# Spaces and punctuation are counted as engraved units.
+
+def cost_of_project(engraving, solid_gold):
+    cost = solid_gold * (100 + 10 * len(engraving)) + (not solid_gold) * (50 + 7 * len(engraving))
+    return cost
