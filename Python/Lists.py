2d4fa948c884c7e55b513b8a84557201de0f3d68
Seunghyun Kim <sevencuter@gmail.com>
2024-12-20 14:09:55 +0900

    Kaggle Python Lesson4 Question1

---
+++ Python/Lists.py
@@ -0,0 +1,14 @@
+# 1.
+# Complete the function below according to its docstring.
+
+def select_second(L):
+    """Return the second element of the given list. If the list has no second
+    element, return None.
+    """
+    pass
+    if len(L) < 2:
+        return None
+
+    return L[1]
+
+
