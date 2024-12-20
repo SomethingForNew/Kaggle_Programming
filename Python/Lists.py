c6b3db9fa968797990e36ab534476b3fd6178738
Seunghyun Kim <sevencuter@gmail.com>
2024-12-20 15:14:47 +0900

    Kaggle Python Lesson4

--- Python/Lists.py
+++ Python/Lists.py
@@ -26,3 +26,54 @@ def losing_team_captain(teams):
     return teams[-1][1]
 
 
+# 3.
+# The next iteration of Mario Kart will feature an extra-infuriating new item, the Purple Shell.
+# When used, it warps the last place racer into first place and the first place racer into last place.
+# Complete the function below to implement the Purple Shell's effect.
+
+def purple_shell(racers):
+    """Given a list of racers, set the first place racer (at the front of the list) to last
+    place and vice versa.
+    
+    >>> r = ["Mario", "Bowser", "Luigi"]
+    >>> purple_shell(r)
+    >>> r
+    ["Luigi", "Bowser", "Mario"]
+    """
+    pass
+    
+    racers[0], racers[-1] = racers[-1], racers[0]
+
+
+# 4.
+# What are the lengths of the following lists? Fill in the variable lengths with your predictions.
+# (Try to make a prediction for each list without just calling len() on it.)
+
+a = [1, 2, 3]
+b = [1, [2, 3]]
+c = []
+d = [1, 2, 3][1:]
+
+# Put your predictions in the list below. Lengths should contain 4 numbers, the
+# first being the length of a, the second being the length of b and so on.
+lengths = [len(a), len(b), len(c), len(d)]
+
+
+# 5. 🌶️
+# We're using lists to record people who attended our party and what order they arrived in.
+# For example, the following list represents a party with 7 guests, in which Adela showed up first and Ford was the last to arrive:
+
+# party_attendees = ['Adela', 'Fleda', 'Owen', 'May', 'Mona', 'Gilbert', 'Ford']
+# A guest is considered 'fashionably late' if they arrived after at least half of the party's guests.
+# However, they must not be the very last guest (that's taking it too far). In the above example, Mona and Gilbert are the only guests who were fashionably late.
+
+# Complete the function below which takes a list of party attendees as well as a person, and tells us whether that person is fashionably late.
+
+def fashionably_late(arrivals, name):
+    """Given an ordered list of arrivals to the party and a name, return whether the guest with that
+    name was fashionably late.
+    """
+    pass
+    
+    order = arrivals.index(name)
+    return order >= len(arrivals) / 2 and order != len(arrivals) - 1
