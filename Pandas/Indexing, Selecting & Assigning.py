ed94f8a4e0fbfe57d2610ff7afd633889a13c017
Seunghyun Kim <sevencuter@gmail.com>
2025-01-07 16:55:20 +0900

    Kaggle Pandas Exercise: Indexing, Selecting & Assigning

--- Pandas/Indexing, Selecting & Assigning.py
+++ Pandas/Indexing, Selecting & Assigning.py
@@ -50,3 +50,28 @@
 # cols = ['country', 'province', 'region_1', 'region_2']
 # indices = [0, 1, 10, 100]
 # df = reviews.loc[indices, cols]
+
+# 7.
+# Create a variable df containing the country and variety columns of the first 100 records.
+
+# Hint: you may use loc or iloc. When working on the answer this question and the several of the ones that follow, keep the following "gotcha" described in the tutorial:
+
+# iloc uses the Python stdlib indexing scheme, where the first element of the range is included and the last one excluded. loc, meanwhile, indexes inclusively.
+
+# This is particularly confusing when the DataFrame index is a simple numerical list, e.g. 0,...,1000. In this case df.iloc[0:1000] will return 1000 entries, while df.loc[0:1000] return 1001 of them! To get 1000 elements using loc, you will need to go one lower and ask for df.iloc[0:999].
+
+# My Solution:
+# cols = ['country', 'variety']
+# df = reviews.loc[:99, cols]
+
+
+# 8.
+# Create a DataFrame italian_wines containing reviews of wines made in Italy. Hint: reviews.country equals what?
+
+# italian_wines = reviews[reviews.country == 'Italy']
+
+
+# 9.
+# Create a DataFrame top_oceania_wines containing all reviews with at least 95 points (out of 100) for wines from Australia or New Zealand.
+
+# top_oceania_wines = reviews.loc[(reviews.country.isin(['Australia', 'New Zealand'])) & (reviews.points >= 95)]
